eedbce0dbbcca170986a3179c2db1f45520905ad
isandeepkumar30 <sandeep@vismaad.com>
2024-02-28 11:29:20 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'SikhVille_Games',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
